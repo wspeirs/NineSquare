19914d4eb827c24b97226cca2e8fab59c86fe8f8
Ben Heiskell <ben.heiskell@gmail.com>
2014-07-12 16:30:16 -0400

    Fixing button, removing unneeded tabs, changing default page to event listings.

--- NineSquareApp/www/js/app.js
+++ NineSquareApp/www/js/app.js
@@ -88,7 +88,7 @@ $logProvider.debugEnabled(true);
     })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/events');
 
 });
 
